89708b2b721d7e43174aa9125fb2dcdb1378f9ff
Prince Tunde Gbadmosi <ptg@oamarkets.local>
2025-12-18 14:56:43 +0000

    Modified

--- MySQLServer.py
+++ MySQLServer.py
@@ -8,21 +8,19 @@ def create_database():
             user = "root",
             password = "my_password_here"
         )
+        if connection.is_connected():
+            cursor = connection.cursor()
+            cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+            print("Database 'alx_book_store' created successfully!")
     
-    if connection.is_connected():
-        cursor = connection.cursor()
-        cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
-        print("Database 'alx_book_store' created successfully!")
-    
-    except Error as e:
+    except mysql.connector.Error as e:
         print(f"Error while connecting to MySQL: {e}")
     
-    finally
+    finally:
         if 'cursor' in locals():
             cursor.close()
         if 'connection' in locals() and connection.is_connected():
             connection.close()
-            
+             
 if __name__ == "__main__":
     create_database()
-    create_
